e86199184f82c52893d3d2d8dbc0ad62571f422e
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-05 17:49:23 -0300

    fix: accept empty string in description

--- src/Models/publishPostModel.js
+++ src/Models/publishPostModel.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 
 export const publishPostSchema = Joi.object({
     link: Joi.string().uri().required(),
-    description: Joi.string().trim()
+    description: Joi.string().allow('')
 })
