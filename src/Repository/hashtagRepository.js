a85a8736bc0bd830cbb281b6ef94b7cd30dbbd35
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-01-06 16:50:09 -0300

    feat: getPostByHashtag brings userInfo

--- src/Repository/hashtagRepository.js
+++ src/Repository/hashtagRepository.js
@@ -2,6 +2,11 @@ import { hash } from "bcrypt";
 import connectionDb from "../Database/db.js";
 
 export function getPostByHashtagRepository (hashtag) {
-    return connectionDb.query(`SELECT * FROM posts WHERE description ILIKE $1`,
+    return connectionDb.query(`SELECT
+    posts.id, posts.link, posts.description, posts.user_id, posts.created_at,
+    users.name, users.photo
+    FROM posts
+    JOIN users ON users.id = posts.user_id
+    WHERE description ILIKE $1`,
     [`%#${hashtag}%`])
 }
