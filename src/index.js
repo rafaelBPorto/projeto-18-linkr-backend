7c7d7bd3e4e706abc23bb96143477533ca175110
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-09 17:38:45 -0300

    fix: port to PORT

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ app.use(authUserRoute);
 app.use(timelineRoute);
 app.use(hashtagRoute);
 
-const port = 4000;
-app.listen(port, () => console.log(`Server runing in port ${port}`));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server is running in port ${port}`));
