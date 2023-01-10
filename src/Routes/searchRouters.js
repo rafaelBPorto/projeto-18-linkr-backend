fe454b1039a0884dafd820a3c2326265521846bb
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-09 21:06:22 -0300

    fix: correction conflit git

--- src/Routes/searchRouters.js
+++ src/Routes/searchRouters.js
@@ -1,2 +1,9 @@
+import { Router } from "express";
+import { search } from "../Controllers/search.controllers.js";
+
+const router = Router();
+
+router.get("/timeline/search", search)
+
 
 export default router;
