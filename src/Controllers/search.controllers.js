b2fbd6c9c94aebbbe7f59e08402f489cd92d161c
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-01-10 11:26:12 -0300

    fix: search route

--- src/Controllers/search.controllers.js
+++ src/Controllers/search.controllers.js
@@ -1,11 +1,11 @@
-import {connectionDb} from "../Database/db.js"
+import connectionDb from "../Database/db.js"
 
 export async function search(req,res){
     const {name} = req.body
 
     try{
         const searching = await connectionDb.query(
-            `SELECT * FROM users WHERE name ILIKE ${name}%`
+            `SELECT * FROM users WHERE name ILIKE $1`, [name]
         )
         res.send(searching.rows)
     }catch(err){
