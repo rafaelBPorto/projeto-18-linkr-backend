ee079a6086e67115b1f8a0cc1ea64f36c93b73a1
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-01-11 15:20:30 -0300

    feat: deslikePost route

--- src/Controllers/likeController.js
+++ src/Controllers/likeController.js
@@ -1,4 +1,4 @@
-import { postLike } from "../Repository/likeRepository.js";
+import { deletePostLike, postLike } from "../Repository/likeRepository.js";
 
 
 export async function postLikeController(req, res) {
@@ -10,6 +10,22 @@ export async function postLikeController(req, res) {
         await postLike(post_id, user_id);
 
         return res.status(200).send("Você curtiu o post");
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
+    }
+}
+
+export async function deleteLikeController(req, res) {
+
+    const {post_id, user_id} = res.locals;
+
+    console.log(post_id)
+    console.log(user_id)
+    try {
+        await deletePostLike(post_id, user_id);
+
+        return res.status(200).send("Você descurtiu o post");
     } catch (err) {
         console.log(err);
         return res.sendStatus(500);
