09990f8f4b29ffa5bb1dc2f72e8710d4399f6af7
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-13 17:43:50 -0300

    chore: change port for deploy

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ app.use(timelineRoute);
 app.use(searchRoute);
 app.use(likeRoute);
 
-const port = 4000;
-app.listen(port, () => console.log(`Server runing in port ${port}`));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server is running in port ${port}`));
